d1e568f3db5319ac1381abb69ce3a6210ddb4218
Austin Ring <aring97@gmail.com>
2020-08-06 09:41:48 -0500

    working on weather

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,9 @@ import { eaterySelect } from "./eateries/EaterySelect.js";
 import { parkSelect } from "./parks/ParkSelect.js";
 import { attractionSelect } from "./attractions/AttractionSelect.js"
 import { ParkList } from "./parks/ParkList.js";
+import { useWeather } from "./weather/WeatherProvider.js";
 parkSelect()
+.then(useWeather())
 eaterySelect()
 attractionSelect()
 ParkList()
--- scripts/parks/ParkProvider.js
+++ scripts/parks/ParkProvider.js
@@ -10,7 +10,7 @@ export const useParks = () => {
 
 
 export const getParks = () => {
-    return fetch(`https://developer.nps.gov/api/v1/parks?api_key=${key}`)
+    return fetch(`https://developer.nps.gov/api/v1/parks?api_key=${key.npsKey}`)
         .then(response => response.json())
         .then(parsedParks => {
             parks = parsedParks.data
